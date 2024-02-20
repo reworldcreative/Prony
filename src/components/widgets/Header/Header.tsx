14736fc4bb312a8b1238007338adeedd3249c462
reworldcreative <reworldcreative@gmail.com>
2024-02-20 18:05:05 +0200

    add Dropdown to storybook

--- src/components/widgets/Header/Header.tsx
+++ src/components/widgets/Header/Header.tsx
@@ -32,7 +32,7 @@ const Header: FC = () => {
           />
         </div>
 
-        <HeaderUserMenu useOption={true} />
+        <HeaderUserMenu useOption={false} />
       </div>
     </header>
   );
